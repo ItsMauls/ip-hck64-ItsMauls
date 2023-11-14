e8738a657bb852de263c87815599278cd13cae4c
Maulana Ibrahim <maulputra09@gmail.com>
2023-11-14 15:46:18 +0700

    latest

---
+++ maukucing-client/src/components/HotPost.jsx
@@ -0,0 +1,17 @@
+export const HotPost = () => {
+    return (
+        <>
+        <div className="bg-red-500 w-1/4 rounded-lg m-2 fixed float-left">
+        <ul>
+            <li className="border-b py-1 black text-center">Hot Post</li>
+            <li className="border-b black">1</li>
+            <li className="border-b black">1</li>
+            <li className="border-b black">1</li>
+            <li className="border-b black">1</li>
+            <li className="border-b black">1</li>
+            <li className="border-b black">1</li>
+        </ul>
+    </div>
+        </>
+    )
+}
---
+++ maukucing-client/src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import { Content } from "../components/Content"
+import { HotPost } from "../components/HotPost"
+
+export const MainPage = () => {
+    return (
+    <>
+    < HotPost/>
+    
+    < Content/>
+    
+    </>
+    )
+}
